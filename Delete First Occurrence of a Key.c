d850be4febb281c66bffcf4453659b0c2cbedc40
Rajat Singh <rajatsinghixd8647@gmail.com>
2026-02-24 21:52:22 +0530

    Add files via upload

---
+++ Delete First Occurrence of a Key.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Node* deleteFirst(struct Node* head, int key) {
+    struct Node *temp = head, *prev = NULL;
+
+    if (temp != NULL && temp->data == key) {
+        head = temp->next;
+        free(temp);
+        return head;
+    }
+
+    while (temp != NULL && temp->data != key) {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    if (temp == NULL)
+        return head;
+
+
+    prev->next = temp->next;
+    free(temp);
+
+    return head;
+}
+
+void printList(struct Node* head) {
+    while (head != NULL) {
+        printf("%d ", head->data);
+        head = head->next;
+    }
+}
+
+int main() {
+    int n, key, x;
+    scanf("%d", &n);
+
+    struct Node *head = NULL, *tail = NULL;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        struct Node* newNode = createNode(x);
+
+        if (head == NULL) {
+            head = tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+
+    scanf("%d", &key);
+
+    head = deleteFirst(head, key);
+
+    printList(head);
+
+    return 0;
+}
