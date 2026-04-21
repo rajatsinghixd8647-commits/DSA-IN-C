9ceba976d07decd145c0b1a1a1056eeb0bc51cbd
Rajat Singh <rajatsinghixd8647@gmail.com>
2026-04-21 08:30:27 +0530

    Create Floyd-Warshall Algorithm.c

---
+++ Floyd-Warshall Algorithm.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <limits.h>
+
+#define INF 1000000000
+
+int main() {
+    int n;
+    
+    // Input number of vertices
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    int dist[n][n];
+
+    // Input adjacency matrix
+    printf("Enter adjacency matrix (-1 for no edge):\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &dist[i][j]);
+
+            if (dist[i][j] == -1 && i != j) {
+                dist[i][j] = INF;
+            }
+        }
+    }
+
+    // Floyd-Warshall Algorithm
+    for (int k = 0; k < n; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dist[i][k] < INF && dist[k][j] < INF) {
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+    }
+
+    // Output shortest distance matrix
+    printf("\nShortest Distance Matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (dist[i][j] == INF)
+                printf("-1 ");
+            else
+                printf("%d ", dist[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
