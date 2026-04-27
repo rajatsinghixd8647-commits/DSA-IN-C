c8fd2852e757fe19512ac05fa2cf5e10874c233c
Rajat Singh <rajatsinghixd8647@gmail.com>
2026-04-27 18:31:56 +0530

    Create Square Root using Binary Search.c

---
+++ Square Root using Binary Search.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    long long n;
+    scanf("%lld", &n);
+
+    long long left = 0, right = n, mid, ans = 0;
+
+    while (left <= right) {
+        mid = left + (right - left) / 2;
+
+        if (mid * mid == n) {
+            ans = mid;
+            break;
+        }
+        else if (mid * mid < n) {
+            ans = mid;
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    printf("%lld", ans);
+
+    return 0;
+}
