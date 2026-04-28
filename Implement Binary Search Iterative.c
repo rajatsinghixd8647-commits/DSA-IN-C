359472efab035a7037346ac2ab3952c821fe5862
Rajat Singh <rajatsinghixd8647@gmail.com>
2026-04-28 19:27:11 +0530

    Create Implement Binary Search Iterative.c

---
+++ Implement Binary Search Iterative.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, minIndex, temp;
+
+    scanf("%d", &n);
+    int arr[n];
+
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Selection Sort
+    for(i = 0; i < n - 1; i++) {
+        minIndex = i;
+
+        for(j = i + 1; j < n; j++) {
+            if(arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+
+    // Print sorted array
+    for(i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
